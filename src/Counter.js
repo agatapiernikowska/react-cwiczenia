821aa4b9110041c76f0935abf17027f0541ce569
Agata Piernikowska <agata.piernikowska@yahoo.com>
2018-05-06 13:55:54 +0200

    Refactoring

--- src/Counter.js
+++ src/Counter.js
@@ -4,17 +4,11 @@ class Counter extends Component {
     value:0
 }
 
-handleIncrementClick = () => this.setState({
-  value: Math.min(this.state.value + 1,10)
+handleClick = delta => this.setState ({
+  value: delta === undefined
+  ?this.props.initialValue
+    :Math.max(Math.min(this.state.value + delta, 10), 0)
 })
-
-  handleDecrementClick = () => this.setState({
-    value: Math.max(this.state.value - 1,0)
-  })
-
-  handleResetClick = () => this.setState({
-    value: this.props.initialValue
-  })
 
   static getDerivedStateFromProps (nextProps, prevState) {
     return {value:nextProps.initialValue}
@@ -25,13 +19,13 @@ handleIncrementClick = () => this.setState({
       <Fragment>
         <h1>{this.state.value}</h1>
         <p>
-          <button onClick={this.handleIncrementClick}>
+          <button onClick={() => this.handleClick(1)}>
             Increment
           </button>
-          <button onClick={this.handleDecrementClick}>
+          <button onClick={() => this.handleClick(-1)}>
             Decrement
           </button>
-          <button onClick={this.handleResetClick}>
+          <button onClick={() => this.handleClick()}>
             Reset
           </button>
         </p>
