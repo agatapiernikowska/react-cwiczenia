c2c408dfbf84ead9df86f0e3df454481f7d67029
Agata Piernikowska <agata.piernikowska@yahoo.com>
2018-05-06 10:51:53 +0200

    Extract AppIntro from App

---
+++ src/AppIntro.js
@@ -0,0 +1,12 @@
+import React from 'react'
+class AppIntro extends React.Component {
+  render (){
+    return (
+      <div className="App-intro">
+        Hello everyone :)
+      </div>
+    )
+  }
+}
+
+export default AppIntro
