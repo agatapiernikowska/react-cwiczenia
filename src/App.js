dd403bd323e3f6876aefb12ec88562bbb239df78
Agata Piernikowska <agata.piernikowska@yahoo.com>
2018-05-06 10:59:52 +0200

    Extract Header from App

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 import AppIntro from "./AppIntro";
+import './App.css';
+import Header from "./Header";
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+
+        <Header/>
         <AppIntro/>
+
       </div>
     );
   }
