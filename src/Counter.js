b6f471f3a6460cd0ca59564e6ca1909464c3bbb7
Agata Piernikowska <agata.piernikowska@yahoo.com>
2018-05-06 13:17:03 +0200

    Buttons (decrement, reset)

--- src/Counter.js
+++ src/Counter.js
@@ -4,16 +4,30 @@ class Counter extends Component {
     value:0
 }
 
-handleClick = () => this.setState({
+handleIncrementClick = () => this.setState({
   value: this.state.value + 1
 })
+
+  handleDecrementClick = () => this.setState({
+    value: this.state.value - 1
+  })
+
+  handleResetClick = () => this.setState({
+    value: 0
+  })
 
   render() {
     return (
       <Fragment>
         <h1>{this.state.value}</h1>
         <p>
-          <button onClick={this.handleClick}>
+          <button onClick={this.handleIncrementClick}>
+            Increment
+          </button>
+          <button onClick={this.handleDecrementClick}>
+            Increment
+          </button>
+          <button onClick={this.handleResetClick}>
             Increment
           </button>
         </p>
